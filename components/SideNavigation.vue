e66ac649313f16e25744dd1e9f7a406c56ff787a
YasuhiroIhara <iharay@gmail.com>
2020-04-30 23:24:42 +0900

    SNSリンクを北九州市公式に更新

--- components/SideNavigation.vue
+++ components/SideNavigation.vue
@@ -51,7 +51,7 @@
       <footer class="SideNavigation-Footer">
         <div class="SideNavigation-Social">
           <a
-            href="https://line.me/R/ti/p/%40822sysfc"
+            href="https://page.line.me/zng7240n/"
             target="_blank"
             rel="noopener noreferrer"
             class="SideNavigation-SocialLink"
@@ -62,7 +62,7 @@
             </picture>
           </a>
           <a
-            href="https://twitter.com/city_kitakyushu"
+            href="https://twitter.com/city_kitakyushu/"
             target="_blank"
             rel="noopener noreferrer"
             class="SideNavigation-SocialLink"
@@ -73,7 +73,7 @@
             </picture>
           </a>
           <a
-            href="https://www.facebook.com/kitakyushu.hokenfukushi"
+            href="https://www.facebook.com/city.of.kitakyushu"
             target="_blank"
             rel="noopener noreferrer"
             class="SideNavigation-SocialLink"
