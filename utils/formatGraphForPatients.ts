1d13da848ca8911ff262f116a6797adeae367889
池田大樹 <ikedahiroki@MacBook-Pro.local>
2020-05-13 20:54:19 +0900

    4/0になるのを修正

--- utils/formatGraphForPatients.ts
+++ utils/formatGraphForPatients.ts
@@ -17,16 +17,22 @@ export type GraphDataType = {
 export default (data: DataType[]) => {
   const graphData: GraphDataType[] = []
   const today = new Date()
+  const yesterday = new Date(
+    today.getFullYear(),
+    today.getMonth(),
+    today.getDate()
+  )
+  console.log(yesterday)
   let patSum = 0
   data
-    .filter(d => new Date(d['日付']) < today)
+    .filter(d => new Date(d['日付']) < yesterday)
     .forEach(d => {
       const date = new Date(d['日付'])
       const subTotal = d['小計']
       if (!isNaN(subTotal)) {
         patSum += subTotal
         graphData.push({
-          label: `${date.getMonth() + 1}/${date.getDate() - 1}`,
+          label: `${date.getMonth() + 1}/${date.getDate()}`,
           transition: subTotal,
           cumulative: patSum
         })
